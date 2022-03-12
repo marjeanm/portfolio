3d23da9ba54401515448d89824c341914ccfbf8f
Marjean Mayo-Baker <marjean_m@hotmail.com>
2022-03-11 22:12:14 -0500

    added remaining files for a full commit

---
+++ js/js.js
@@ -0,0 +1,29 @@
+/*smooth scroll*/
+const arrow = document.querySelectorAll('.scroll-link');
+for (const arrows of arrow) {
+  arrows.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = e.currentTarget.getAttribute('href');
+    console.log(id);
+    const element = document.getElementById(id);
+    console.log(element);
+    let position = element.offsetTop;
+    console.log(position);
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+  });
+}
+const navH = document.getElementById('nav');
+const topLink = document.querySelector('.top-link');
+window.addEventListener('scroll', function () {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navH.getBoundingClientRect().height;
+  console.log(navHeight);
+  if (scrollHeight > 100) {
+    topLink.classList.add('show-link');
+  } else {
+    topLink.classList.remove('show-link');
+  }
+});
